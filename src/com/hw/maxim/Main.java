7b3b5ce82665ebc26fc2b74c9133add431f803a9
maks <maxim_86nva@mail.ru>
2023-05-06 13:27:14 +0200

    строки

--- src/com/hw/maxim/Main.java
+++ src/com/hw/maxim/Main.java
@@ -3,6 +3,28 @@ package com.hw.maxim;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+//        String fullName = lastName + " " + firstName + " " + middleName;
+//        System.out.println("ФИО сотрудника - " + fullName);
+
+        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+        String answer1 = String.format("ФИО сотрудника - %s", fullName);
+        System.out.println(answer1);
+        System.out.println();
+
+        System.out.println("Задача 2");
+        String fullNameInUpperCase = fullName.toUpperCase();
+        String answer2 = String.format("Данные ФИО сотрудника для заполнения отчета — %s", fullNameInUpperCase);
+        System.out.println(answer2);
+        System.out.println();
+
+        System.out.println("Задача 3");
+        String fullNameWithE = "Иванов Семён Семёнович";
+        String fullNameWithoutE = fullNameWithE.replace('ё', 'е');
+        System.out.println(fullNameWithoutE);
     }
 }
